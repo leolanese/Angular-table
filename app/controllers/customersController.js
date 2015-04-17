879cada8279312ebed38cc432c8748a0dfb07d5c
Leo Lanese <developer@leolanese.com>
2015-04-17 14:41:32 +0100

    using best practices
    reformating according John's Papa best practices

--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,68 +1,79 @@
-var CustomersController = function ($scope) {
+;(function () {
+  'use strict';
 
-    $scope.sortBy = 'name';
-    $scope.reverse = false;
+  angular
+    .module('customersApp')
+    .controller('CustomersController', CustomersController);
 
-    $scope.customers= [
-            {
-                country: 'Germany',
-                branch: 'Audi',
-                model: 'Avanti',
-                price: '90000'
-            },
-            {
-                country: 'Italy',
-                branch: 'Fiat',
-                model: '600',
-                price: '12000'
-            },
-            {
-                country: 'France',
-                branch: 'Peugeout',
-                model: '450',
-                price: '14000'
-            },
-            {
-                country: 'Italy',
-                branch: 'Ferrari',
-                model: 'Testarossa',
-                price: '45000'
-            },
-            {
-                country: 'Germany',
-                branch: 'Mercedes',
-                model: 'M3',
-                price: '67000'
-            },
-            {
-                country: 'Germany',
-                branch: 'Audi',
-                model: 'A3',
-                price: '27000'
-            },
-            {
-                country: 'India',
-                branch: 'Terrafugia',
-                model: 'TF-X',
-                price: '127000'
-            },
-            {
-                country: 'England',
-                branch: 'Volvo',
-                model: 'X3',
-                price: '16000'
-            }];
+  /* @ngInject */
+  CustomersController.$inject = [
+    'dialogService'
+  ];
 
-    $scope.doSort = function(propName) {
-       $scope.sortBy = propName;
-       $scope.reverse = !$scope.reverse;
-    };
+  CustomersController.$inject = ['$scope'];
 
-};
+  function CustomersController() {
 
-CustomersController.$inject = ['$scope'];
+      var vm = this;
 
-angular.module('customersApp')
+      vm.sortBy = 'name';
+      vm.reverse = false;
 
-.controller('CustomersController', CustomersController);
+      vm.doSort = function(propName) {
+        vm.sortBy = propName;
+        vm.reverse = !vm.reverse;
+      };
 
+      vm.customers= [
+        {
+          country: 'Germany',
+          branch: 'Audi',
+          model: 'Avanti',
+          price: '90000'
+        },
+        {
+          country: 'Italy',
+          branch: 'Fiat',
+          model: '600',
+          price: '12000'
+        },
+        {
+          country: 'France',
+          branch: 'Peugeout',
+          model: '450',
+          price: '14000'
+        },
+        {
+          country: 'Italy',
+          branch: 'Ferrari',
+          model: 'Testarossa',
+          price: '45000'
+        },
+        {
+          country: 'Germany',
+          branch: 'Mercedes',
+          model: 'M3',
+          price: '67000'
+        },
+        {
+          country: 'Germany',
+          branch: 'Audi',
+          model: 'A3',
+          price: '27000'
+        },
+        {
+          country: 'India',
+          branch: 'Terrafugia',
+          model: 'TF-X',
+          price: '127000'
+        },
+        {
+          country: 'England',
+          branch: 'Volvo',
+          model: 'X3',
+          price: '16000'
+        }];
+
+  }
+
+})();
